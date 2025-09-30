b0fe113242daafee033f4e2b64602a2fb5480bd0
martakhmil <martakhmil.11@gmail.com>
2025-09-30 23:45:23 +0200

    Додала код у другий файл

---
+++ ai_11/marta_khmil/epic_1/practice_work_task_1_marta_khmil.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double p, r, t, a;
+    int n;
+
+    cout << "Enter the initial amount (P): ";
+    cin >> p;
+
+    cout << "Enter the annual interest rate (%): ";
+    cin >> r;
+
+    cout << "Enter the number of times interest is compounded per year: ";
+    cin >> n;
+
+    cout << "Enter the number of years: ";
+    cin >> t;
+
+    a = p * pow(1 + (r / 100) / n, n * t);
+
+    cout << "Final amount (A): " << a << endl;
+
+    return 0;
+}
