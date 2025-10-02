0a66b6ac08dbdb24a9d203ad744598fc9e1fcac7
DmytroPatsula <dmytropatsula@gmail.com>
2025-10-02 19:45:09 +0300

    Epic 1 - Dmytro Patsula

---
+++ ai_11/dmytro_patsula/epic_1/practice_work_task_1_dmytropatsula.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <cmath>
+
+int main() {
+
+    double p, r, A, profit;
+int n, t;
+
+printf("Your investment:");
+scanf("%lf", &p);
+if(p <= 0) {
+    printf("Invalid input");
+    return 0;
+    
+}
+printf("Yearly interest rate (in percentage): ");
+scanf("%lf", &r);
+if (r < 0 || r > 100) {
+        printf("Invalid input");
+    return 0;
+}
+r = r / 100;
+
+printf("Number of interest accruals per year(1,4,12):");
+scanf("%d", &n);
+if(n == 1 || n == 4 || n == 12) {}
+
+else{
+     printf("Invalid input");
+return 0;
+}
+
+printf("Number of years:");
+scanf("%d", &t);
+if(t <= 0) {
+    printf("Invalid input");
+    return 0;
+    
+}
+
+A = p * pow((1 + r / n), n * t);
+profit = A - p;
+
+printf("You invested \"%.2f\"", p);
+printf(" at a yearly interest rate of \"%.2d%%\"", (int)(r*100));
+printf(" compounded \"%d\" times per year", n);
+printf(" for \"%d\" years.\n", t);
+printf(" The total amount after interest is \"%.2f\".\n", A);
+printf(" Your profit is \"%.2f\".", profit);
+
+    return 0;
+}
