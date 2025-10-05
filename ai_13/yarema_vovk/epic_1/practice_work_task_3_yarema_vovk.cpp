39f56a52bd89b4dcc8a3226f310ea464249179c8
Yarema <yaremator11072008@gmail.com>
2025-10-05 17:40:50 +0300

    Доробив звіт, зремонтував 3 таск

--- ai_13/yarema_vovk/epic_1/practice_work_task_3_yarema_vovk.cpp
+++ ai_13/yarema_vovk/epic_1/practice_work_task_3_yarema_vovk.cpp
@@ -4,23 +4,23 @@
 #include <fcntl.h>
 #include <cmath>
 
-int main(){
+int main() {
     _setmode(_fileno(stdout), _O_U16TEXT);
     _setmode(_fileno(stdin), _O_U16TEXT);
 
     int views;
     int increase;
     int days;
-    double projecion;
+    long double projection;
 
-    wprintf(L"Перегляди, зростання, дні: ");
+    wprintf(L"Перегляди, зростання (%%), дні: ");
     wscanf(L"%d %d %d", &views, &increase, &days);
 
-    projecion = views * pow(1 + double(increase)/100, days);
-    wprintf(L"Views: %d, Increase: %d, Days: %d\n", views, increase, days);
+    projection = views * pow(1 + (long double)increase / 100.0, days);
 
-    wprintf(L"Зростання (float): %.2lf\n", projecion);
-    wprintf(L"Прогноз переглядів через %d днів: %.0lf", days, projecion);
+    wprintf(L"\nПерегляди: %d, Зростання: %d%%, Дні: %d\n", views, increase, days);
+    wprintf(L"Прогноз переглядів через %d днів: %.0Lf\n", days, projection);
+    wprintf(L"Зростання (float): %.2Lf\n", projection);
 
     return 0;
 }
