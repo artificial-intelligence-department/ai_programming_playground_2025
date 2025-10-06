3e9cb2cadfdace63fbc53c6976e2d3dffb88abbc
AskoldKushta <askold.kushna.shi.2025@lpnu.ua>
2025-10-06 20:58:03 +0300

    edit files (2)

--- ai_12/askold_kushta/epic_1/practice_work_task_1_askold_kushta.cpp
+++ ai_12/askold_kushta/epic_1/practice_work_task_1_askold_kushta.cpp
@@ -5,32 +5,33 @@ using namespace std;
 
  int main(){
     
-    cout << "Deposited sum: ";
-    int dep=0;
-    cin >> dep;
+   int dep=0;
+   double rate=0;
+   int time=0;
+
+   cout << "Deposited sum: ";
+   cin >> dep;
     
-    cout << "Interest rate (%): ";
-    double rate=0;
-    cin >> rate;
-    rate=rate/100;
+   cout << "Interest rate (%): ";
+   cin >> rate;
+    
+   cout << "Amount of time (years): ";
+   cin >> time;
 
-    cout << "Amount of time (years): ";
-    int time=0;
-    cin >> time;
-
-    if(dep<0 || !(0<=rate<=1) || time<0){
+   if(dep<0 || !(0<=rate<=100) || time<0){
       cout << "Неправильні введені дані" << endl;
       return 1;
-    }
-    
-    double final=dep*pow(1+rate, time);
-    cout << "Final sum: " << final << endl;
-    cout << "Profit: " << final-dep << endl;
+   }
 
-    cout << "Payments:" << endl;
-    cout << "Monthly: " << (final-dep)/(time*12) << endl;
-    cout << "Quarterly: " << (final-dep)/(time*4) << endl;
-    cout << "Yearly: " << (final-dep)/time << endl;
+   rate=rate/100;
+   double final=dep*pow(1+rate, time);
+   cout << "Final sum: " << final << endl;
+   cout << "Profit: " << final-dep << endl;
+
+   cout << "Payments:" << endl;
+   cout << "Monthly: " << (final-dep)/(time*12) << endl;
+   cout << "Quarterly: " << (final-dep)/(time*4) << endl;
+   cout << "Yearly: " << (final-dep)/time << endl;
     
-    return 0;
+   return 0;
  }
