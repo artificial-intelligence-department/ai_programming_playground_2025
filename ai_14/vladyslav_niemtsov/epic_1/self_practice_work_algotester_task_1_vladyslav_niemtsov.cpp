29e7230b8e04b3ecf25e243ec808adf3b604b090
Vladyslav Niemtsov <vladnyemts@gmail.com>
2025-10-07 23:28:04 +0300

    added solution for algotester

---
+++ ai_14/vladyslav_niemtsov/epic_1/self_practice_work_algotester_task_1_vladyslav_niemtsov.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a, b, c;
+    cin >> a >> b >> c;
+
+    if (a + b > c)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
