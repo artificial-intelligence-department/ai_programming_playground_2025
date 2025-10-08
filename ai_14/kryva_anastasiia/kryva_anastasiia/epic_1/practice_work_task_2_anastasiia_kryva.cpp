2072bb56d996c28d859060a4ab7ce26d76c8da7e
nkrvd <anastasiia.kryva.shi.2025@lpnu.ua>
2025-10-08 12:31:08 +0200

    Видалено practice_work_task_2.cpp

--- ai_14/kryva_anastasiia/kryva_anastasiia/epic_1/practice_work_task_2_anastasiia_kryva.cpp
+++
@@ -1,6 +0,0 @@
-﻿Build started at 12:07...
-1>------ Build started: Project: practice_work_task_2_anastasiia_kryva.cpp, Configuration: Debug x64 ------
-1>practice_work_task_2_anastasiia_kryva.cpp.cpp
-1>practice_work_task_2_anastasiia_kryva.cpp.vcxproj -> C:\Users\user\Downloads\Нова папка\practice_work_task_2_anastasiia_kryva.cpp\x64\Debug\practice_work_task_2_anastasiia_kryva.cpp.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build completed at 12:07 and took 01,913 seconds ==========
