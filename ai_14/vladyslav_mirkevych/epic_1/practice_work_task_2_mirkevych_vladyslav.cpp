59e9bf6f9c4b7992f968ae32aca43ed2c7870f7d
Vladyslav Mirkevych <mirkevicvlad21@gmail.com>
2025-10-08 15:15:34 +0300

    поправив код дописав звіт

--- ai_14/vladyslav_mirkevych/epic_1/practice_work_task_2_mirkevych_vladyslav.cpp
+++ ai_14/vladyslav_mirkevych/epic_1/practice_work_task_2_mirkevych_vladyslav.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std ;
 int main()
 { 
     string name;
     int workedhours;
-    double stavka;
-    cin >> name >> workedhours >> stavka;
-    cout << name << " " << stavka*workedhours;
+    double salaryrate;
+    cout << "Write name -";
+    cin >> name;
+    cout << "Write workedhours - ";
+    cin >> workedhours;
+    cout << "Write rate - ";
+    cin >> salaryrate;
+    cout << "Name - " <<name << "\nsalary : "  <<fixed << setprecision(2)<< salaryrate*workedhours;
     return 0;
      
 }
