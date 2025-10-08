7336fe04fdf7b488d09b1365a1725f92e8500f59
nkrvd <anastasiia.kryva.shi.2025@lpnu.ua>
2025-10-08 12:33:39 +0200

    Додано practice_work_task_1

---
+++ ai_14/kryva_anastasiia/kryva_anastasiia/epic_1/practice_work_task_1_anstasiia_kryva.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath> // for pow()
+using namespace std;
+
+int main() {
+    double P, r, t; // initial amount, annual rate, time
+    int n; // number of times interest is compounded per year
+
+    cout << "Enter the initial amount (P): ";
+    cin >> P;
+    cout << "Enter the annual interest rate (r in %): ";
+    cin >> r;
+    cout << "Enter the number of compounding periods per year (n): ";
+    cin >> n;
+    cout << "Enter the number of years (t): ";
+    cin >> t;
+
+    double A = P * pow(1 + (r / (100 * n)), n * t);
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout << "Future investment value (A): " << A << " UAH" << endl;
+
+    return 0;
+}
