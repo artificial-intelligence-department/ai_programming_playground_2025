b57616154e5da917468e27f56c7c867a33eb7eeb
nkrvd <anastasiia.kryva.shi.2025@lpnu.ua>
2025-10-08 12:42:25 +0200

    Додано Task 2 для Epic 1

---
+++ ai_14/kryva_anastasiia/kryva_anastasiia/epic_1/practice_work_task_2_anastasiia.kryva.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    int hours;
+    double rate;
+
+    cout << "Enter employee name: ";
+    cin >> name;
+    cout << "Enter number of worked hours: ";
+    cin >> hours;
+    cout << "Enter hourly rate: ";
+    cin >> rate;
+
+    double salary = hours * rate;
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout << "Employee " << name << " earned: " << salary << " UAH" << endl;
+
+    return 0;
+}
