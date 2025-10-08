784424c6adfae38c07f7f34be3a7b020f039f380
MochiBao <soneshka189@gmail.com>
2025-10-08 20:08:54 +0300

    update task algotester

--- ai_14/sofiia_marchynska/epic_1/self_practice_work_algotester_task_1_sofiia_marchynska.cpp
+++ ai_14/sofiia_marchynska/epic_1/self_practice_work_algotester_task_1_sofiia_marchynska.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main () {
-    int a, b;
-    cin >> a >> b;
-    cout << a + b;
+    int n, number = 0;
+    cin >> n;
+    int money [9] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+    for (int i = 0; i < 9; i++) {
+        number += n / money[i];
+        n = n %money[i];
+    };
+    cout << number << endl;
     return 0;
-}
+};
