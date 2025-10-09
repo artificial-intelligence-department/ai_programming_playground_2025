b1271ca84bd0c75299144c2141bf604862f8756e
veronikavasylyk12 <veronika.vasylyk.shi.2025@lpnu.ua>
2025-10-10 01:16:43 +0300

    epic_1_practice_and_labs_report_veronika_vasylyk.docx

--- ai_14/veronika_vasylyk/epic_1/practice_work_task_3_veronika_vasylyk.cpp
+++ ai_14/veronika_vasylyk/epic_1/practice_work_task_3_veronika_vasylyk.cpp
@@ -3,10 +3,28 @@
 using namespace std;
 
 int main() {
-    int P, n, t, A;
-    double r=0.05;
-    printf("Введіть початкову суму, кількість нарахувань за рік і роки:\n");
-    scanf("%d%d%d", &P, &n, &t);
+    int P, n, t;
+    double r=0.05, A;
+    printf("Введіть початкову суму\n");
+    scanf("%d", &P);
+    printf("Введіть термін вкладу (в роках)\n");
+    scanf("%d", &t);
+    printf("Виберіть варіант нарахування відсотків\n");
+    printf("1 Кожного місяця:\n");
+    printf("2 Кожного кварталу:\n");
+    printf("3 Кожного року:\n");
+    printf("Ваш вибір: ");
+    scanf("%d", &n);
+    if (n == 1) {
+    n = 12;
+    } else if (n == 2) {
+    n = 4;
+    } else if (n == 3) {
+    n = 1;
+    } else {
+        printf("Невірний вибір!\n");
+        return 1;
+    }
     A = P * pow(1 + r/n, n*t);
     printf("Вкладені кошти %d\n", P);
     printf("Загальна сума %d\n", (int)round(A));
