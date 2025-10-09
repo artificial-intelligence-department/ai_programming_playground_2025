4c5d21aee3ad64f4b305361abb017975c453707e
DmytroPatsula <dmytropatsula@gmail.com>
2025-10-09 18:24:37 +0300

    Changed two files

--- ai_11/dmytro_patsula/epic_1/self_practice_work_algotester_task_1_dmytropatsula.cpp
+++ ai_11/dmytro_patsula/epic_1/self_practice_work_algotester_task_1_dmytropatsula.cpp
@@ -3,12 +3,10 @@ using namespace std;
 
 int main() {
  int n;
- cout << "Enter number of biscuits packs:";
  cin >> n;
 long long sum = 0;
 for (int i = 0; i < n; ++i) {
     long long x;
-    cout << "Enter number of biscuits in pack " << i + 1 << ":";
     cin >> x;
     sum += x - 1;
 }
