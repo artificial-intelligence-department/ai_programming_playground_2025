17e606ac551aa43ed22fc36612107cfcc6766a96
demiankropyvnytskyishi2025-cpu <demian.kropyvnytskyi.shi.2025@lpnu.ua>
2025-10-10 10:22:33 +0300

    update file

--- ai_11/demian_kropyvnytskyy/EPIC-1/practice_work_task_1_demian_kropyvnytskyi.cpp
+++ ai_11/demian_kropyvnytskyy/EPIC-1/practice_work_task_1_demian_kropyvnytskyi.cpp
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <cmath>
+#include <windows.h>
 int main(){
+    SetConsoleOutputCP(65001);
     printf("Напишіть скільки грошей ви вклали: ");
-    int P;
-    scanf("%d", &P);
+    int p;
+    scanf("%d", &p);
     printf("Річну відсоткову ставку: ");
     float r;
     scanf("%f", &r);
     r = r / 100;
     printf("Кількість років: ");
     int t;
+    
     scanf("%d", &t);
     int n;
     printf("Кількість нарахувань відсотків за рік: ");
     scanf("%d", &n);
     
-    float A = P * pow((1 + r / n), n * t);
-    printf("Сума накопичених коштів: %.2f\n", A);
-    printf("Заробіток: %.2f\n", A - P);
+    float a = p * pow((1 + r / n), n * t);
+    printf("Сума накопичених коштів: %.2f\n", a);
+    printf("Заробіток: %.2f\n", a - p);
     return 0;
 }
