657deeba3b50c6f8dc4f67e6e4b01317a7234c88
Hidei Danylo <danjahidei@gmail.com>
2025-10-10 09:40:56 +0300

    Changing language in task 2

--- ai_11/danylo_hidei/epic_1/practice_work_task_2_danylo_hidei.cpp
+++ ai_11/danylo_hidei/epic_1/practice_work_task_2_danylo_hidei.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
-#include <windows.h>
-
+#include <cstdio>
 
 int main() {
-    setlocale(LC_ALL, "ukr");
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
 
     char name[50];
-    printf("Введіть ім'я: ");
+    printf("Enter name: ");
     scanf("%49s", name);
 
     int hours;
-    printf("Введіть кількість годин: ");
+    printf("Enter hours worked: ");
     scanf("%d", &hours);
 
     float rate;
-    printf("Введіть погодинну ставку: ");
+    printf("Enter hourly rate: ");
     scanf("%f", &rate);
 
     float pay = hours * rate;
-    printf("Працівник %s отримав зарплату:  %.2f\n", name, pay);
+    printf("The employee %s got salary of:  %.2f\n", name, pay);
 
     return 0;
 }
